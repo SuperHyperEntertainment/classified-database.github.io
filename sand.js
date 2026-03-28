0776400fb94e2753972a8a6188453c6709dbc508
HexxedDev <goldepatrick35@gmail.com>
2026-03-28 18:27:02 -0500

    Update sand.js

--- sand.js
+++ sand.js
@@ -1,5 +1,18 @@
-window.getSandColor = () => `hsl(${40 + Math.random() * 10}, ${70 + Math.random() * 20}%, ${50 + Math.random() * 20}%)`;
+window.getSandColor = () => {
+    const h = 40 + Math.random() * 10;
+    return `hsl(${h}, 70%, ${50 + Math.random() * 15}%)`;
+};
 
 window.updateSand = function(x, y, grid, nextGrid, cols, rows) {
-    window.fallSolid(x, y, grid, nextGrid, cols, rows);
+    let cell = grid[x][y];
+    // Straight down
+    if (y + 1 < rows && nextGrid[x][y+1].type === window.EMPTY) {
+        nextGrid[x][y+1] = cell; nextGrid[x][y] = {type: window.EMPTY};
+    } else {
+        // Diagonals (makes cones)
+        let d = Math.random() < 0.5 ? 1 : -1;
+        if (x+d >= 0 && x+d < cols && y+1 < rows && nextGrid[x+d][y+1].type === window.EMPTY) {
+            nextGrid[x+d][y+1] = cell; nextGrid[x][y] = {type: window.EMPTY};
+        }
+    }
 };
